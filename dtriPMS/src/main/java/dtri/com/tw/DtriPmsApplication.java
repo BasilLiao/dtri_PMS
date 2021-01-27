a989c67e403c1831283ed39b11a3829c3831cdde
Basil <q255156@gmail.com>
2021-01-27 15:17:55 +0800

    PMS 初次建置

---
+++ dtriPMS/src/main/java/dtri/com/tw/DtriPmsApplication.java
@@ -0,0 +1,13 @@
+package dtri.com.tw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtriPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtriPmsApplication.class, args);
+	}
+
+}
---
+++ dtriPMS/src/test/java/tw/com/DtriPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtriPmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
