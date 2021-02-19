a928995149259238fad2864b165a12282e7fe155
Basil <q255156@gmail.com>
2021-02-19 14:44:41 +0800

    try

--- dtriPMS/src/main/java/dtri/com/tw/DtriPmsApplication.java
+++ dtriPMS/src/main/java/dtri/com/tw/DtriPmsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DtriPmsApplication {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		SpringApplication.run(DtriPmsApplication.class, args);
 	}
 
